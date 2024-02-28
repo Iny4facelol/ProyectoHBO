f878aea4f7913e12e16bc9f892ebb47f3321fc61
Iny4facelol <150298787+Iny4facelol@users.noreply.github.com>
2024-02-28 14:30:04 +0100

    First

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://iny4facelol.github.io/ProyectoHBO/',
+  base: '/ProyectoHBO',
   integrations: [tailwind()]
 });
